32771278a75eb0058bdc068f10872d33bff5b1ca
jhancd10 <jhancarlos1972@gmail.com>
2020-06-03 16:55:05 -0500

    Add section: Meseros, Estadisticas

---
+++ AplicacionWeb/ClientApp/src/app/estadisticas/estadistica.ts
@@ -0,0 +1,10 @@
+export interface IConsultaVentasMeseros {
+  fechaVentasMeseros: string;
+}
+
+export interface IVentasMesero {
+  idMesero: string;
+  nombres: string;
+  apellidos: string;
+  total: number;
+}
---
+++ AplicacionWeb/Controllers/EstadisticasController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestauranteService;
+
+namespace WebApp_Restautant.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstadisticasController : ControllerBase
+    {
+        [HttpPost]
+        [HttpGet("{fechaInicio}")]
+        public IEnumerable<VentasMesero> Get([FromRoute] string fechaInicio)
+        {
+            DateTime fecha = Convert.ToDateTime(fechaInicio);
+            ServiceClient serviceClient = new ServiceClient();
+            var getList = serviceClient.GetVentasMesero(fecha);
+            var castToList = getList.AsEnumerable().ToList();
+
+            List<VentasMesero> ventasMeseros = new List<VentasMesero>();
+            for (int i = 0; i < castToList.Count; i++)
+            {
+                ventasMeseros.Add(new VentasMesero()
+                {
+                    IdMesero = castToList[i].IdMesero,
+                    Nombres = castToList[i].Nombres,
+                    Apellidos = castToList[i].Apellidos,
+                    Total = castToList[i].Total
+                });
+            }
+            return ventasMeseros;
+        }
+    }
+}
